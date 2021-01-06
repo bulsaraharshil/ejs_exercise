536301a8446d68ab0173e752ee1d5591c16e9b25
bulsaraharshil <bulsaraharshil@gmail.com>
2021-01-06 20:17:19 +0530

    first commit

---
+++ Chessboard.js
@@ -0,0 +1,13 @@
+let size = 8;
+let board = "";
+for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
+        if ((i + j) % 2 == 1) {
+            board += "#"
+        } else {
+            board += " "
+        }
+    }
+    board += "\n"
+}
+console.log(board);
