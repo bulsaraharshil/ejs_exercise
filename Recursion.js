47c8426cdb3df1106b7d1a4d0ec1dd4c0ab7f517
bulsaraharshil <bulsaraharshil@gmail.com>
2021-01-07 20:23:58 +0530

    converted n to absolute number which helps for -ve numbers

--- Recursion.js
+++ Recursion.js
@@ -1,13 +1,12 @@
 function isEven(n) {
+    n = Math.abs(n)
     if (n == 0) {
         return true;
     } else if (n == 1) {
         return false;
-    } else if (n < 0) {
-        return isEven(-n)
     } else {
         return isEven(n - 2)
     }
 }
 
-console.log(isEven(50));
+console.log(isEven(-2));
