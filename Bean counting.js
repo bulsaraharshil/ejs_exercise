0bda746198087464e56931372eb628232fc5e9a0
bulsaraharshil <bulsaraharshil@gmail.com>
2021-01-07 20:26:42 +0530

    changes

--- Bean counting.js
+++ Bean counting.js
@@ -11,5 +11,15 @@ function countChar(string, char) {
 function countBs(string) {
     return countChar(string, "B");
 }
-console.log(countChar("harshil", "h"))
-console.log(countBs("ABBCD"));
+
+//----ANOTHER WAY(NEW WAY)----
+// var countBs = function(str) {
+//     return str.match(/B/g).length;
+// };
+
+// var countChar = function(str, character) {
+//     var matchExp = new RegExp(character, 'g');
+//     return str.match(matchExp).length;
+// };
+// console.log(countChar("harshil", "h"))
+// console.log(countBs("ABBCD"));
